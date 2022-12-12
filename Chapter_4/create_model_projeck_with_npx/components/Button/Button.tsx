a38697f7c66885b5e32860b9f1d317e988ce5d1f
Dmitrii Kolca <leonidi4@list.ru>
2022-12-12 21:41:48 +0200

    Chapter 7. Lesson 4,5,6. Add custom Button component.

---
+++ Chapter_4/create_model_projeck_with_npx/components/Button/Button.tsx
@@ -0,0 +1,20 @@
+import {ButtonProps} from "./Button.props";
+import styles from "./Button.module.css";
+import cn from 'classnames';
+
+
+export const Button = ({appearance, children, className, ...props}: ButtonProps): JSX.Element => {
+    return (
+        <button
+            className={cn(styles.button, {
+                [styles.primary]: appearance == 'primary',
+                [styles.ghost]: appearance == 'ghost',
+            })}
+            {...props}
+        >
+
+            {children}
+        </button>
+    );
+
+}
--- Chapter_4/create_model_projeck_with_npx/pages/_app.tsx
+++ Chapter_4/create_model_projeck_with_npx/pages/_app.tsx
@@ -6,7 +6,7 @@ export default function App({Component, pageProps}: AppProps): JSX.Element {
     return <>
         <Head>
             <title> Обучение TS </title>
-            <link rel="icon" href="/favicon.ico"/>
+            <link rel="icon" href="/dfWoyL96cZc.jpg"/>
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
 
             <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap"
