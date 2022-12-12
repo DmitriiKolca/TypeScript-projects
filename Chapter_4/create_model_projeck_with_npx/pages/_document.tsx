10ae4cdf0f1cd0f3044dfdd0cd928f87812518c3
Dmitrii Kolca <leonidi4@list.ru>
2022-12-12 12:02:45 +0200

    Chapter 4, creating and setting up a project with npx create_next_app

---
+++ Chapter_4/create_model_projeck_with_npx/pages/_document.tsx
@@ -0,0 +1,21 @@
+import Document, {DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript} from "next/document";
+
+
+export default class MyDocument extends Document {
+    static async getInitialPrors(context: DocumentContext): Promise<DocumentInitialProps> {
+        const initialProps = await Document.getInitialProps(context);
+        return {...initialProps};
+    }
+
+    render(): JSX.Element {
+        return (
+            <Html lang="ru">
+                <Head/>
+                <body>
+                <Main/>
+                <NextScript/>
+                </body>
+            </Html>
+        );
+    }
+}
