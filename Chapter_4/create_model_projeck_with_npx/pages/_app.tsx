ebee51ce7d11062a469fb3f14afd6720e87c00ed
Dmitrii Kolca <leonidi4@list.ru>
2022-12-12 17:58:18 +0200

    Chapter 7. Lesson 1,2,3. Add First Component.

--- Chapter_4/create_model_projeck_with_npx/pages/_app.tsx
+++ Chapter_4/create_model_projeck_with_npx/pages/_app.tsx
@@ -7,6 +7,10 @@ export default function App({Component, pageProps}: AppProps): JSX.Element {
         <Head>
             <title> Обучение TS </title>
             <link rel="icon" href="/favicon.ico"/>
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+
+            <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap"
+                  rel="stylesheet"/>
         </Head>
         <Component {...pageProps}/>
     </>
