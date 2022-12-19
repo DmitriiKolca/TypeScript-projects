02ff1f4440e5cb586e39f345e1e0f45d9f88a1b7
Dmitrii Kolca <leonidi4@list.ru>
2022-12-19 16:34:17 +0200

    Chapter 10. Layout grid footer and adaptive footer

--- Chapter_4/create_model_projeck_with_npx/layout/Footer/Footer.tsx
+++ Chapter_4/create_model_projeck_with_npx/layout/Footer/Footer.tsx
@@ -1,10 +1,14 @@
 import {FooterProps} from "./Footer.props";
+import styles from "./Footer.module.css"
+import cn from "classnames";
+import {format} from 'date-fns'
 
-
-export const Footer = ({...props}: FooterProps): JSX.Element => {
+export const Footer = ({className, ...props}: FooterProps): JSX.Element => {
     return (
-        <div {...props}>
-            Footer
-        </div>
+        <footer {...props} className={cn(className, styles.footer)}>
+            <div>OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены</div>
+            <a href="#" target="_blank">Пользовательское соглашение</a>
+            <a href="#" target="_blank" className="text">Политика конфиденциальности</a>
+        </footer>
     )
 }
