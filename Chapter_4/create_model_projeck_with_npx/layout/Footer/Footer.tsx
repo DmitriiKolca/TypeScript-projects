4c65801fe82ddb51bcdfe1dc9bf8ba07d3525096
Dmitrii Kolca <leonidi4@list.ru>
2022-12-19 15:31:56 +0200

    Chapter 10. Layout width grid

--- Chapter_4/create_model_projeck_with_npx/layout/Footer/Footer.tsx
+++ Chapter_4/create_model_projeck_with_npx/layout/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import {FooterProps} from "./Footer.props";
 export const Footer = ({...props}: FooterProps): JSX.Element => {
     return (
         <div {...props}>
-            Header
+            Footer
         </div>
     )
 }
